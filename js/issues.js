bd8397f8db7ea2692352ef7c1c2d4f59ea942cf5
Joost De Cock <joost@decock.org>
2017-07-06 09:12:10 +0000

    Added GitHub issues overview

---
+++ js/issues.js
@@ -0,0 +1,35 @@
+(function ($) {
+    $(document).ready(function () {
+        var repos = ['core', 'data', 'site'];
+        $.each(repos, function( index, repo ) {
+            $.get('https://api.github.com/repos/freesewing/'+repo+'/issues', function( issues ) {
+                $('#'+repo+'-count').html(issues.length);
+                if(issues.length > 0) {
+                    $('#'+repo+'-issues').html('<p>'+issues.length+' open issues in <a href="https://github.com/freesewing/'+repo+'" target="_BLANK">'+repo+'</a></p>');
+                    $.each(issues, function( index, issue ) {
+                        $('#'+repo+'-issues').append(renderIssue(issue));
+                    });
+                    timeago().render($('.timeago'));
+                } else {
+                    $('#'+repo+'-issues').html('<p>No open issues in <a href="https://github.com/freesewing/'+repo+'" target="_BLANK">'+repo+'</a></p>');
+                }
+            });
+        });
+
+        function renderIssue(issue) {
+            console.log(issue);
+            var html = '<div class="mb-4 drop-shadow paper" id="'+issue.number+'">';
+            html += '<p class="mb-0 py-0"><small>Issue <a href="#'+issue.number+'">#'+issue.number+'</a>, ';
+            html += 'created <span class="timeago" datetime="'+issue.created_at+'"></span> by ';
+            html += '<a href="'+issue.user.html_url+'" target="_BLANK">'+issue.user.login+'</a></small></p>';
+            html += '<h4 class="mt-0 py-0"><a href="'+issue.html_url+'" target="_BLANK">'+issue.title+'</a></h4>';
+            html += '<div class="content" style="overflow-x: auto;">'+marked(issue.body)+'</div>';
+            $.each(issue.labels, function( index, label ) {
+                html += '<span class="badge" style="background: #'+label.color+';">'+label.name+'</span>';
+            });
+            html += '<p class="mb-0 mt-4"><small>'+issue.comments+' comments, last update <span class="timeago" datetime="'+issue.updated_at+'"></span></small></p>';
+            html += '</div>';
+            return html;
+        }
+    });
+}(jQuery));
---
+++ pages/issues.md
@@ -0,0 +1,22 @@
+---
+layout: cards
+title: Issues
+tags: [contributors]
+permalink: /issues
+action: issues
+---
+<div class="container">
+<div class="col-md-8 offset-md-2">
+<p>Skip to: 
+<a class="mx-1" href="#core">core issues (<span id="core-count"></span>)</a>
+<a class="mx-1" href="#data">data issues (<span id="data-count"></span>)</a>
+<a class="mx-1" href="#site">site issues (<span id="site-count"></span>)</a>
+</p>
+<h2 id="core">Core issues</h2>
+<div id="core-issues"><img src="/img/logo/spinner.svg"></div>
+<h2 id="data">Data issues</h2>
+<div id="data-issues"><img src="/img/logo/spinner.svg"></div>
+<h2 id="site">Site issues</h2>
+<div id="site-issues"><img src="/img/logo/spinner.svg"></div>
+</div>
+</div>
