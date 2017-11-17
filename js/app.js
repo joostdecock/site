8ed6251348b6e0c38acd354af718a7b0a9b45bd2
Joost De Cock <joost@decock.org>
2017-11-17 12:42:26 +0000

    Added check for undefined data.social

--- js/app.js
+++ js/app.js
@@ -1875,9 +1875,10 @@
                     $('ul.breadcrumbs li:last-child').html(data.profile.username);
                     $('#avatar').attr('src', api.data+data.profile.pictureSrc);
                     $('span.username').html(data.profile.username);
-                    if(typeof data.social.twitter != 'undefined') $('#social').append('<a href="https://twitter.com/'+data.social.twitter+'" target="_BLANK" title="'+data.profile.username+' is '+data.social.twitter+' on Twitter" class="px-2"><i class="fa fa-twitter fa-4x" aria-hidden="true"></i></a>');
-                    if(typeof data.social.instagram != 'undefined') $('#social').append('<a href="https://instagram.com/'+data.social.instagram+'" target="_BLANK" title="'+data.profile.username+' is '+data.social.instagram+' on Instagram" class="px-2"><i class="fa fa-instagram fa-4x" aria-hidden="true"></i></a>');
-                    console.log(typeof data.social.twitter);
+                    if(typeof data.social != 'undefined') {
+                        if(typeof data.social.twitter != 'undefined') $('#social').append('<a href="https://twitter.com/'+data.social.twitter+'" target="_BLANK" title="'+data.profile.username+' is '+data.social.twitter+' on Twitter" class="px-2"><i class="fa fa-twitter fa-4x" aria-hidden="true"></i></a>');
+                        if(typeof data.social.instagram != 'undefined') $('#social').append('<a href="https://instagram.com/'+data.social.instagram+'" target="_BLANK" title="'+data.profile.username+' is '+data.social.instagram+' on Instagram" class="px-2"><i class="fa fa-instagram fa-4x" aria-hidden="true"></i></a>');
+                    }
                     $.each(data.badges, function(name, val){
                         $('#badges').append('<a href="/docs/site/badges#'+name+'"><img src="/img/badges/badge-'+name+'.svg" class="badge-img drop-shadow" style="margin: 5px;"></a>');
                     });
