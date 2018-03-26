6b229645bc823dda0d12654e6bfdf333fc402504
Joost De Cock <joost@decock.org>
2018-03-26 14:14:31 +0700

    Commit prior to nuxtent hacking

--- app/pages/_page.vue
+++ app/pages/_page.vue
@@ -1,17 +1,25 @@
 <template>
-<section>
-    <router-link to="/">Home</router-link> / 
-    {{ page.title }}
-  <h1>{{ page.title }}</h1>
-  <nuxtent-body :body="page.body" />
-</section>
+  <section>
+    <AppBreadcrumbs crumbs="breadcrumbs" />
+    <article>
+      <h1>{{ page.title }}</h1>
+      <nuxtent-body :body="page.body" />
+    </article>
+  </section>
 </template>
 
 <script>
+import AppBreadcrumbs from '~/components/Navigation/AppBreadcrumbs'
 export default {
     asyncData: async function ({ app, route }) {
         console.log('Getting '+route.path+' from content api')
         return { page: await app.$content('/pages').get(route.path)}
+    },
+    components: {
+      AppBreadcrumbs
+    },
+    mounted: function() {
+      console.log(this.$data.page.meta) 
     }
 }
 </script>
