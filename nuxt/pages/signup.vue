52f241b4a560ae4cfcf4776326c9f08d47a5bddb
Joost De Cock <joost@decock.org>
2018-04-11 15:21:59 +0000

    Work on signup page

--- nuxt/pages/signup.vue
+++ nuxt/pages/signup.vue
@@ -5,36 +5,46 @@
       <v-btn @click="logout" color="secondary" large>Logout</v-btn>
     </div>
     <div v-else>
+      <h1>{{ $t('signUp') }}</h1>
+      <h4>{{ $t('txt-signup-step1') }}</h4>
       <v-form v-model="valid">
         <v-text-field
-          :label="$t('email')"
-          v-model="email"
-          required
-        ></v-text-field>
-        <v-text-field
-          :label="$t('username')"
-          v-model="username"
-          required
-        ></v-text-field>
-        <v-text-field
-          :label="$t('password')"
-          v-model="password"
-          type="password"
-          required
-        ></v-text-field>
-        <v-btn @click="submit" color="primary" large>
-          {{ $t('login') }}
-          <v-progress-circular indeterminate color="#fff" class="ml-4" v-if="loading" :size="20" :width="2"></v-progress-circular>
-        </v-btn>
+              :label="$t('email')"
+              v-model="email"
+              required
+              prepend-icon="email"
+              ></v-text-field>
+          <v-text-field
+              :label="$t('password')"
+              v-model="password"
+              :append-icon="hidePassword ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (hidePassword = !hidePassword)"
+              :type="hidePassword ? 'password' : 'text'"
+              required
+              prepend-icon="vpn_key"
+              ></v-text-field>
+            <v-btn @click="submit" color="primary" large>
+              {{ $t('signUp') }}
+              <v-progress-circular indeterminate color="#fff" class="ml-4" v-if="loading" :size="20" :width="2"></v-progress-circular>
+            </v-btn>
       </v-form>
+
+        <p>
+        * {{ $t('txt-email-sharing') }}
+        <br>
+        * {{ $t('txt-password-policy') }}
+        </p>
+      <ul class="steps mt-5">
+        <li><v-icon>check_circle</v-icon>{{ $t('txt-signup-step1') }}</li>
+      </ul>
     </div>
     <v-snackbar
-      :timeout="(4000)"
-      top
-      right
-      v-model="error"
-      >{{ $t('loginFailed') }}
-      <v-btn flat color="primary" @click.native="error = false"><v-icon>close</v-icon></v-btn>
+          :timeout="(4000)"
+          top
+          right
+          v-model="error"
+          >{{ $t('loginFailed') }}
+          <v-btn flat color="primary" @click.native="error = false"><v-icon>close</v-icon></v-btn>
     </v-snackbar>
   </section>
 </template>
@@ -46,14 +56,15 @@ export default {
   data () {
     return {
       email: '',
-      username: '',
+      password: '',
       valid: false,
       loading: false,
-      error: false
+      error: false,
+      hidePassword: true
     }
   },
   mounted: () => { 
-  //  console.log($auth.user)
+    //  console.log($auth.user)
   },
   computed: { 
     loggedIn () {
@@ -69,13 +80,13 @@ export default {
           password: this.password
         }
       })
-      .catch((i) => {
-        this.loading = false;
-        this.error = true
-      })
-      .then((i) => {
-        this.loading = false;
-      })
+        .catch((i) => {
+          this.loading = false;
+          this.error = true
+        })
+        .then((i) => {
+          this.loading = false;
+        })
     },
     logout: function() {
       this.$auth.logout()
@@ -86,8 +97,18 @@ export default {
 </script>
 
 <style scoped>
-  form {
-    max-width: 300px;
-    margin: auto;
-  }
+form {
+  max-width: 300px;
+  margin: auto;
+}
+.input-group--required label::after {
+  content: ''!important;
+}
+ul.steps {
+  margin-left: 0;
+  font-size: 70%;
+}
+ul.steps li {
+  list-style-type: none;
+}
 </style>
