a7773f0c3e827fa381db51d2b81a5514bd2d1f74
Joost De Cock <joost@decock.org>
2018-04-27 08:54:20 +0200

    Fixed button component markup

--- nuxt/components/stateless/FsButtonLogin.vue
+++ nuxt/components/stateless/FsButtonLogin.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="px-3 fs-nodeco">
-    <nuxt-link :to="$fs.path('/login')" class="txt-xs-center">
-      <v-btn large block color="primary">
-        <v-icon class="mr-3" color="white">vpn_key</v-icon>
-        {{ $t('logIn') }}
-      </v-btn>
-    </nuxt-link>
-  </div>
+  <v-btn color="primary" :to="$fs.path('/login')" class="nodeco">
+    <v-icon class="mr-3" color="white">vpn_key</v-icon>
+    {{ $t('logIn') }}
+  </v-btn>
 </template>
 
--- nuxt/components/stateless/FsButtonPatron.vue
+++ nuxt/components/stateless/FsButtonPatron.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="px-3 fs-nodeco">
-    <nuxt-link :to="$fs.path('/patrons/join')" class="txt-xs-center">
-      <v-btn large block color="accent">
-        <v-icon class="mr-3" color="white">favorite</v-icon>
-        {{ $t('becomeAPatron') }}
-      </v-btn>
-    </nuxt-link>
-  </div>
+ <v-btn color="accent" class="nodeco" :to="$fs.path('/patrons/join')">
+   <v-icon class="mr-3" color="white">favorite</v-icon>
+   {{ $t('becomeAPatron') }}
+ </v-btn>
 </template>
 
