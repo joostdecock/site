dd2ffc0adbed97c6b5fa315d62ca7e06ff864fdf
joostdecock <joost@decock.org>
2018-07-04 09:02:51 +0000

    :globe_with_meridians: i18n for 404 and error pages

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -1,12 +1,17 @@
 <template>
   <div style="margin: auto; text-align: center;">
     <span v-if="error.statusCode === 404" style="font-size: 7vw; font-weight: 900; line-height: 1; display: block;">404</span>
-    <span v-else style="font-size: 7vw; font-weight: 900; line-height: 1; display: block;">¯\_(ツ)_/¯</span>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <p>Return to <nuxt-link :to="$fs.path('/')">the homepage</nuxt-link> or
-    <a href="https://github.com/freesewing/site/issues/new">report this error</a>.</p>
-    <nuxt-link :to="$fs.path('/')"><img src="/img/patterns/simon/cover.jpg" style="max-height: 50vh;"></nuxt-link>
+    <span v-else style="font-size: 7vw; font-weight: 900; line-height: 1.5; display: block;">¯\_(ツ)_/¯</span>
+    <h1 v-if="error.statusCode === 404">{{ $t('pageNotFound') }}</h1>
+    <h1 v-else>{{ $t('error-unknown') }}</h1>
+    <p>
+      <nuxt-link :to="$fs.path('/')">{{ $t('home') }}</nuxt-link>
+      <span class="px-3">|</span>
+      <a href="https://github.com/freesewing/site/issues/new">{{ $t('reportThisOnGitHub') }}</a>
+    </p>
+    <nuxt-link :to="$fs.path('/')">
+      <img src="/img/patterns/simon/cover.jpg" style="max-height: 50vh;">
+    </nuxt-link>
   </div>
 </template>
 
